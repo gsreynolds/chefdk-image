b2dbf834ded7dfd2838beeec6c3df0e1dcfaf8eb
Gavin Reynolds <gavin@chef.io>
2019-05-13 12:19:33 +0100

    Add jumpstart kitchen template
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- scripts/chef-jumpstart-workstation.ps1
+++ scripts/chef-jumpstart-workstation.ps1
@@ -1,5 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y chef-workstation vscode conemu googlechrome
 
+Invoke-WebRequest "https://raw.githubusercontent.com/gsreynolds/chefdk-image/chef-jumpstart/test_kitchen-templates/kitchen-template-jumpstart.yml" -OutFile 'C:\\Users\\Administrator\kitchen-template.yml'
+
 Write-Output "Setting Administrator Password"
 ([ADSI]'WinNT://localhost/Administrator, user').psbase.Invoke('SetPassword', 'Cod3Can!')
